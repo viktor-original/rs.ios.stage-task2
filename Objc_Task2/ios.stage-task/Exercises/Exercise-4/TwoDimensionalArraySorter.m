2037d6e3dc6f38f21c26e37fa230ee80e9f35e7e
viktor-original <viktor.originaly@gmail.com>
2021-05-18 02:43:34 +0300

    feat: exercise 4 - done

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,58 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    
+    NSMutableArray* numArray = [NSMutableArray new];
+    NSMutableArray* strArray = [NSMutableArray new];
+    
+    if ([array isEqualToArray:@[]] || array == nil || ![array[0] isKindOfClass:[NSArray class]])  {
+            return @[];
+        }
+    
+    
+    for (NSArray* lilArray in array) {
+        for (id unknown in lilArray) {
+            if ([unknown isKindOfClass:[NSNumber class]]) {
+                [numArray addObject:unknown];
+            } else {
+                [strArray addObject:unknown];
+            }
+        }
+        
+    }
+    
+    if ([strArray count] == 0) {
+        
+        [numArray sortUsingDescriptors:
+        [NSArray arrayWithObject:
+        [NSSortDescriptor sortDescriptorWithKey:@"self"
+                                      ascending:YES]]];
+        
+        return numArray;
+        
+    }
+    
+    if ([numArray count] == 0) {
+        
+        [strArray sortUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
+        
+        return strArray;
+        
+    }
+    
+    [strArray sortUsingDescriptors:
+     [NSArray arrayWithObject:
+      [NSSortDescriptor sortDescriptorWithKey:nil
+                                    ascending:NO
+                                     selector:@selector(localizedCaseInsensitiveCompare:)]]];
+    
+    [numArray sortUsingDescriptors:
+     [NSArray arrayWithObjects:
+      [NSSortDescriptor sortDescriptorWithKey:nil
+                                    ascending:YES], nil]];
+    
+    return [NSArray arrayWithObjects:numArray, strArray, nil];
+    
 }
 
 @end
